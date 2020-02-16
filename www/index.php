69e50bb82a8755ea762220ef854147e289aaaa1e
Ivan Sokolin <bratislavka21@mail.ru>
2020-02-16 18:20:28 +0300

    Изменен www/index.php, добавлена автозагрузка классов
    Добавлена автозагрузка классов в index.php

--- www/index.php
+++ www/index.php
@@ -1,1 +1,13 @@
 <?php
+
+function myAutoLoader(string $className)
+{
+    require_once __DIR__ . '/../src/' . $className . '.php';
+}
+
+spl_autoload_register('myAutoLoader');
+
+$author = new MyProject\Models\Users\User('Tigran');
+$article = new MyProject\Models\Articles\Article('some title', 'some text', $author);
+
+var_dump($article);
