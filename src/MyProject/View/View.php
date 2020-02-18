bb0218cb2db538a4aef250619fb65dbd26df01dd
Ivan Sokolin <bratislavka21@mail.ru>
2020-02-18 23:05:38 +0300

    Добавлена буферизация вывода в src/MyProject/View/View::renderHtml()

--- src/MyProject/View/View.php
+++ src/MyProject/View/View.php
@@ -16,7 +16,12 @@ class View
     public function renderHtml(string $templateName, array $vars = [])
     {
         extract($vars);
-
+        
+		ob_start();
         include $this->templatesPath . '/' . $templateName;
+		$buffer = ob_get_contents();
+		ob_end_clean();
+		
+		echo $buffer;
     }
 }
