66c770275ea38ee28c3d61bc15cc90db2191708c
Ivan Sokolin <bratislavka21@mail.ru>
2020-02-18 23:20:10 +0300

    Шаблон для главной страницы разбит на составляющие
    Изменен файл templates/main/main. Теперь он не содержит, а подключает
    хедер и футер.
    Добавлены файлы templates/header и templates/footer.

---
+++ templates/header.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <title>Блог</title>
+  <link href="/styles.css" rel="stylesheet">
+</head>
+<body>
+  <table class="layout">
+    
+	<tr>
+	  <td colspan="2" class="header">
+	    Майн блог
+	  </td>
+	</tr>
+	
+	<tr>
+	  <td>
--- templates/main/main.php
+++ templates/main/main.php
@@ -1,41 +1,7 @@
-<!doctype html>
-<html lang="ru">
-<head>
-  <meta charset="UTF-8">
-  <title>Блог</title>
-  <link href="/styles.css" rel="stylesheet">
-</head>
-<body>
-  <table class="layout">
-    
-	<tr>
-	  <td colspan="2" class="header">
-	    Майн блог
-	  </td>
-	</tr>
-	
-	<tr>
-	  <td>
-	    <?php foreach ($articles as $article): ?>
-	      <h2><?= $article['name'] ?></h2>
-		  <p><?= $article['text'] ?></p>
-		  <hr>
-		<?php endforeach; ?>  
-	  </td>
-	  
-	  <td width="300px" class="sidebar">
-	    <div class="sidebarHeader">Меню</div>
-		<ul>
-		  <li><a href="/">Главная страница</a></li>
-		  <li><a href="/about-me">Обо мне</a></li>
-		</ul>
-	  </td>
-	</tr>
-	
-    <tr>
-      <td colspan="2" class="footer">Копирайт (с) Майн блог</td>
-	</tr>
-	
-  </table>
-</body>
-</html>
+<?php include __DIR__ . '/../header.php'; ?>
+<?php foreach ($articles as $article): ?>
+  <h2><?= $article['name'] ?></h2>
+  <p><?= $article['text'] ?></p>
+  <hr>
+<?php endforeach; ?>  
+<?php include __DIR__ . '/../footer.php'; ?>
