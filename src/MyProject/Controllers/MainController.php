50f2d20d4252fc34ec8f5ea5a24f69633b539384
Ivan Sokolin <bratislavka21@mail.ru>
2020-02-18 22:50:11 +0300

    Добавлен класс View. Класс View вызван в MainController::main()
    Добавлен файл src/MyProject/View/View.
    Новый класс вызван в src/MyProject/Controllers/MainController::main().

--- src/MyProject/Controllers/MainController.php
+++ src/MyProject/Controllers/MainController.php
@@ -2,15 +2,24 @@
 
 namespace MyProject\Controllers;
 
+use MyProject\View\View;
+
 class MainController
 {
-	public function main()
+    private $view;
+
+    public function __construct()
+    {
+        $this->view = new View(__DIR__ . '/../../../templates');
+    }
+
+    public function main()
 	{
 		$articles = [
 			['name' => 'Статья 1', 'text' => 'Текст первой статьи'],
 			['name' => 'Статья 2', 'text' => 'Текст второй статьи'],
 		];
-		require __DIR__ . '/../../../templates/main/main.php';
+		$this->view->renderHtml('main/main.php', ['articles' => $articles]);
 	}
 	
 	public function sayHello(string $name)
