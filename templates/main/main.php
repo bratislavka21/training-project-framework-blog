07c788fe52ece6d9896ddb3a70bf8be0150e26c5
Ivan Sokolin <bratislavka21@mail.ru>
2020-02-18 15:47:30 +0300

    Добавлен html-шаблон для главной страницы
    Добавлен файл templates/main/main.php с html-шаблоном
    Добавлен файл www/styles.css для стилизации

---
+++ templates/main/main.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <title>Блог</title>
+  <link href="/styles.css" rel="stylesheet">
+</head>
+<body>
+  <table class="layout">
+    
+	<tr>
+	  <td colspan="2" class="header">
+	    Майн блог
+	  </td>
+	</tr>
+	
+	<tr>
+	  <td>
+	    <h2>Статья 1</h2>
+		<p>Текст первой статьи</p>
+		<hr>
+		<h2>Статья 2</h2>
+		<p>Текст второй статьи</p>
+		<hr>
+	  </td>
+	  
+	  <td width="300px" class="sidebar">
+	    <div class="sidebarHeader">Меню</div>
+		<ul>
+		  <li><a href="/">Главная страница</a></li>
+		  <li><a href="/about-me">Обо мне</a></li>
+		</ul>
+	  </td>
+    <tr>
+      <td colspan="2" class="footer">Копирайт (с) Майн блог</td>
+	</tr>
+	
+  </table>
+</body>
+</html>
