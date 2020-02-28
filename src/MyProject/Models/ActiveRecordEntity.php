8376ceccf58df190ec5c635d18b45b5a2c1755c4
Ivan Sokolin <bratislavka21@mail.ru>
2020-02-28 19:04:09 +0300

    В ActiveRecordEntity добавлены методы для сохранения сущности в БД
        save() получает массив св-в и значений этих св-в вызывающей его сущности с именами св-в в формате столбцов в БД. Затем разделяет логику сохранения на случай новой сущности (для инсерта) и уже существующей сущности (для апдейта), и вызывает нужный метод для сохранения сущности в базу.
        mapPropertiesToDbFormat() возвращает массив св-в вызывающего объекта в формате имя_поля_в_бд => значение.
        camelCaseToUnderscore() принимает строку в camelCase, а возвращает эту строку в формате для сохранения в БД.
        update() принимает массив св-в и значений объекта и, на основании принятых св-в составляет SQL-строку для UPDATE-запроса в базу данных, затем совершает запрос на обновление в БД.
        Также был изменен модификатор св-ва id с private на protected.

--- src/MyProject/Models/ActiveRecordEntity.php
+++ src/MyProject/Models/ActiveRecordEntity.php
@@ -7,7 +7,7 @@ use MyProject\Services\Db;
 
 abstract class ActiveRecordEntity
 {
-    private $id;
+    protected $id;
 
     public function __set($name, $value)
     {
@@ -40,10 +40,65 @@ abstract class ActiveRecordEntity
         return $this->id;
     }
 
+    public function save(): void
+    {
+        $mappedProperties = $this->mapPropertiesToDbFormat();
+
+        if ($this->id !== null) {
+            $this->update($mappedProperties);
+        } else {
+            $this->insert();
+        }
+    }
+
     abstract protected static function getTableName(): string;
+
+    private function camelCaseToUnderscore(string $camelCaseName): string
+    {
+        return strtolower(preg_replace('~(?<!^)[A-Z]~', '_$0', $camelCaseName));
+    }
+
+    private function insert()
+    {
+        // здесь будет код для вставки в бд
+    }
+
+    private function mapPropertiesToDbFormat(): array
+    {
+        $reflector = new \ReflectionObject($this);
+        $properties = $reflector->getProperties();
+
+        $mappedProperties = [];
+        foreach ($properties as $property) {
+            $propertyCamelCaseName = $property->getName();
+            $propertyDbName = $this->camelCaseToUnderscore($propertyCamelCaseName);
+            $mappedProperties[$propertyDbName] = $this->$propertyCamelCaseName;
+        }
+        return $mappedProperties;
+    }
 
     private function underscoreToCamelCase(string $origin): string
     {
         return lcfirst(str_replace('_', '',ucwords($origin, '_')));
+    }
+
+    private function update(array $mappedProperties): void
+    {
+        $stringsForSql = [];
+        $placeholdersForQuery = [];
+        $index = 1;
+
+        foreach ($mappedProperties as $name => $value) {
+            $param = ':param' . $index;
+            $index++;
+            $stringsForSql[] = "`$name`=$param";
+            $placeholdersForQuery[$param] = $value;
+        }
+
+        $sql = "UPDATE " . static::getTableName() . " SET " . implode(', ', $stringsForSql) .
+            " WHERE `id` = " . $this->id;
+
+        $db = Db::getInstance();
+        $db->query($sql, $placeholdersForQuery);
     }
 }
