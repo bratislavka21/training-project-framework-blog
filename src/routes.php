8ffab652dc1af650a0009b17e0e883b14d598cdd
Ivan Sokolin <bratislavka21@mail.ru>
2020-02-29 14:00:30 +0300

    В src/routes.php добавлен роут для добавления новой статьи

--- src/routes.php
+++ src/routes.php
@@ -4,5 +4,6 @@ return [
     '~^hello/(.*)$~' => [\MyProject\Controllers\MainController::class, 'sayHello'],
     '~^$~' => [\MyProject\Controllers\MainController::class, 'main'],
     '~^articles/(\d+)$~' => [\MyProject\Controllers\ArticlesController::class, 'view'],
-    '~^articles/edit/(\d+)$~' => [\MyProject\Controllers\ArticlesController::class, 'edit']
+    '~^articles/edit/(\d+)$~' => [\MyProject\Controllers\ArticlesController::class, 'edit'],
+    '~^articles/add/?$~' => [\MyProject\Controllers\ArticlesController::class, 'add']
 ];
