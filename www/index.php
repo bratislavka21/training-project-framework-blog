694fca3829a435f2720ee11e91005f5c4724d6fd
Ivan Sokolin <bratislavka21@mail.ru>
2020-03-01 17:36:58 +0300

    Код фронт-контроллера обернут в блок try-catch
    Теперь в файле www/index.php ловятся исключения типа DbException. В блоке catch добавлен рендеринг шаблона для DbException.

--- www/index.php
+++ www/index.php
@@ -1,33 +1,38 @@
 <?php
 
-function myAutoLoader(string $className)
-{
-    require_once __DIR__ . '/../src/' . $className . '.php';
+try {
+    function myAutoLoader(string $className)
+    {
+        require_once __DIR__ . '/../src/' . $className . '.php';
+    }
+
+    spl_autoload_register('myAutoLoader');
+
+    $route = $_GET['route'] ?? '';
+    $routes = require __DIR__ . '/../src/routes.php';
+
+    $isRouteFound = false;
+    foreach ($routes as $pattern => $controllerAndAction) {
+        preg_match($pattern, $route, $matches);
+        if (!empty($matches)) {
+            $isRouteFound = true;
+            break;
+        }
+    }
+
+    if (!$isRouteFound) {
+        echo 'Страница не найдена';
+        return;
+    }
+
+    unset($matches[0]);
+
+    $controllerName = $controllerAndAction[0];
+    $actionName = $controllerAndAction[1];
+
+    $controller = new $controllerName();
+    $controller->$actionName(...$matches);
+} catch (\MyProject\Exceptions\DbException $e) {
+    $view = new \MyProject\View\View(__DIR__ . '/../templates');
+    $view->renderHtml('errors/500.php', ['error' => $e->getMessage()], 500);
 }
-
-spl_autoload_register('myAutoLoader');
-
-$route = $_GET['route'] ?? '';
-$routes = require __DIR__ . '/../src/routes.php';
-
-$isRouteFound = false;
-foreach ($routes as $pattern => $controllerAndAction) {
-	preg_match($pattern, $route, $matches);
-	if (!empty($matches)) {
-		$isRouteFound = true;
-		break;
-	}
-}
-
-if (!$isRouteFound) {
-	echo 'Страница не найдена';
-	return;
-}
-
-unset($matches[0]);
-
-$controllerName = $controllerAndAction[0];
-$actionName = $controllerAndAction[1];
-
-$controller = new $controllerName();
-$controller->$actionName(...$matches);
