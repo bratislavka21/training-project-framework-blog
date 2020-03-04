2fff008a3f9a4684393b86f8f9ab9eb723bf8393
Ivan Sokolin <bratislavka21@mail.ru>
2020-03-04 21:05:32 +0300

    Дополнен класс UsersController
    Добавлен метод activate(), который сверяет код активации в пользовательском
    входящем запросе и в БД.
    Изменен метод signUp() - теперь он отсылает email-сообщение пользователю со ссылкой
    для активации.

--- src/MyProject/Controllers/UsersController.php
+++ src/MyProject/Controllers/UsersController.php
@@ -4,7 +4,9 @@ namespace MyProject\Controllers;
 
 use MyProject\Exceptions\InvalidArgumentException;
 use MyProject\Models\Users\User;
+use MyProject\Models\Users\UserActivationService;
 use MyProject\View\View;
+use MyProject\Services\EmailSender;
 
 class UsersController
 {
@@ -13,6 +15,16 @@ class UsersController
     public function __construct()
     {
         $this->view = new View(__DIR__ . '/../../../templates');
+    }
+
+    public function activate(int $userId, string $activationCode)
+    {
+        $user = User::getById($userId);
+        $isCodeValid = UserActivationService::checkActivationCode($user, $activationCode);
+        if ($isCodeValid) {
+            $user->activate();
+            echo 'OK!';
+        }
     }
 
     public function signUp()
@@ -26,6 +38,15 @@ class UsersController
             }
 
             if ($user instanceof User) {
+                $code = UserActivationService::createActivationCode($user);
+
+                EmailSender::send(
+                    $user,
+                    'Активация',
+                    'userActivation.php',
+                    ['userId' => $user->getId(), 'code' => $code]
+                );
+
                 $this->view->renderHtml('users/signUpSuccessful.php');
                 return;
             }
