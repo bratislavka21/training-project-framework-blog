38f8969514075f1427d442719084a7bdd10d9a88
Ivan Sokolin <bratislavka21@mail.ru>
2020-03-12 22:03:43 +0300

    Добавлен шаблон "Ошибка авторизации"
      Путь: /templates/errors/401.php

---
+++ templates/errors/401.php
@@ -0,0 +1,4 @@
+<?php include __DIR__ . '/../header.php'; ?>
+    <p>Ошибка авторизации</p>
+    Чтобы просмотреть содержимое страницы, <a href="/users/login/">войдите на сайт</a>
+<?php include __DIR__ . '/../footer.php'; ?>
