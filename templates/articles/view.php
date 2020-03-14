7f84e11ee65b6ad83592e6c4c38f43e948877b63
Ivan Sokolin <bratislavka21@mail.ru>
2020-03-14 18:05:05 +0300

    Изменен шаблон для рендеринга одной страницы /templates/articles/view.php
      В шаблон добавлена ссылка на редактирование текущей статьи, но только если
    пользователь авторизован и пользователь админ.

--- templates/articles/view.php
+++ templates/articles/view.php
@@ -2,4 +2,7 @@
     <h2><?= $article->getName() ?></h2>
     <p><i><?= 'Автор: ' . $article->getAuthor()->getNickname(); ?></i></p>
     <p><?= $article->getText() ?></p>
+    <?php if ($user !== null && $user->isAdmin() === true): ?>
+        <a href="/articles/edit/<?= $article->getId() ?>">Редактировать</a>
+    <?php endif; ?>
 <?php include __DIR__ . '/../footer.php';
