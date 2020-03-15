2fd60b4296e374eb2e236043cfc35f2ec599e4d6
Ivan Sokolin <bratislavka21@mail.ru>
2020-03-15 22:35:59 +0300

    Добавлен абстрактный класс \MyProject\Cli\AbstractCommand
      Класс Summator отнаследован от AbstractCommand.

--- src/MyProject/Cli/Summator.php
+++ src/MyProject/Cli/Summator.php
@@ -4,36 +4,16 @@ namespace MyProject\Cli;
 
 use MyProject\Exceptions\CliException;
 
-class Summator
+class Summator extends AbstractCommand
 {
-    private $params;
-
-    public function __construct(array $params)
-    {
-        $this->params = $params;
-        $this->checkParams();
-    }
-
     public function execute()
     {
         return $this->getParam('a') + $this->getParam('b');
     }
 
-    private function checkParams()
+    protected function checkParams()
     {
         $this->ensureParamExists('a');
         $this->ensureParamExists('b');
-    }
-
-    private function ensureParamExists(string $paramName)
-    {
-        if (!isset($this->params[$paramName])) {
-            throw new CliException('Param "' . $paramName . '" is not set');
-        }
-    }
-
-    private function getParam(string $paramName)
-    {
-        return $this->params[$paramName] ?? null;
     }
 }
