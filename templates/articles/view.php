d6352438488e2f62f7eb76fe5d669d3a522a2dc9
Ivan Sokolin <bratislavka21@mail.ru>
2020-03-18 15:30:48 +0300

    В шаблоне для вывода одной статьи изменен вывод текста
      Путь: /templates/articles/view.php
      Теперь текст прогоняется через markdown-парсер.

--- templates/articles/view.php
+++ templates/articles/view.php
@@ -1,5 +1,5 @@
 <?php include __DIR__ . '/../header.php'; ?>
     <h2><?= $article->getName() ?></h2>
     <p><i><?= 'Автор: ' . $article->getAuthor()->getNickname(); ?></i></p>
-    <p><?= $article->getText() ?></p>
+    <p><?= $article->getParsedText() ?></p>
 <?php include __DIR__ . '/../footer.php';
